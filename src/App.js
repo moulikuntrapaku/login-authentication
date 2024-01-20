95d209267178716ad2672a45d86aea30bf043c9a
moulikuntrapaku <moulikuntrapaku@gmail.com>
2024-01-20 10:23:05 +0530

    Add static login form with styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import './App.css'
+
+
 function App() {
   return (
     <div className="App">
-      
+
+      <div className="welcome-back">
+        <span>Welcome back! 👋</span>
+      </div>
+
+      <header>
+        <span>Sign in to your account</span>
+      </header>
+
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Your email</label>
+          <input type="email" id="email"/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password"/>
+        </div>
+
+        <button type="submit">CONTINUE</button>
+      </form>
+
     </div>
   );
 }
